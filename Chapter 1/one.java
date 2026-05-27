d65f2d0ac61583ed6c8d0b12eaae5ae6da37edc1
vivek sharma <vivek12sharma18@gmail.com>
2026-05-27 17:52:11 +0530

    how to write in console and data types

---
+++ Chapter 1/one.java
@@ -0,0 +1,25 @@
+public class one {
+
+  public static void main (){
+    System.out.print("hello world");
+
+
+    // save data in variable and printing in new line
+    int num1 = 5;
+    int num2 = 10;
+    System.out.println(num1 * num2);
+
+    // Primitive data types
+    int num3 =9;
+    byte byt = 127;
+    short sh = 558;
+    long l = 5854l;
+
+    float f = 5.8f;
+    double d = 3.44;
+
+    char c = 'a';
+    boolean b = true;
+
+  }
+}
